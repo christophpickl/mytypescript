7c305abb76f7b44884127847080cf565ca91a4a8
christophpickl <christoph.pickl@gmail.com>
2018-12-20 18:22:37 +0100

    initial commit

---
+++ src/main/kotlin/com/github/christophpickl/mytypescript/MytypescriptApplication.kt
@@ -0,0 +1,12 @@
+package com.github.christophpickl.mytypescript
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MytypescriptApplication
+
+fun main(args: Array<String>) {
+	runApplication<MytypescriptApplication>(*args)
+}
+
